af8d1bcbc1674a8cbd3139a7902b739aac15b619
Francisco Júnior <jralbbuquerque@gmail.com>
2020-05-11 19:20:51 -0300

    Finish API REST

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,7 +16,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'pg'),
 
   /*
   |--------------------------------------------------------------------------
@@ -53,7 +53,7 @@ module.exports = {
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
+      user: Env.get('DB_USER', 'postgres'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis')
     },
